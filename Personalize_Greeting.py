873b0a58830e2af8449b16a4356efd92087f1d9f
Dhruv Bansal <dhruv.bansal2811@gmail.com>
2025-12-17 20:27:46 +0530

    Add files via upload

---
+++ Personalize_Greeting.py
@@ -0,0 +1,4 @@
+first_name=str(input("Enter your first name: "))
+last_name=str(input("Enter your last name: "))
+total_name=first_name+" "+last_name+"!"
+print("Hello,",total_name,"Welcome to the Python program")
